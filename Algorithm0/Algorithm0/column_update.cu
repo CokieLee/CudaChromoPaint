85e18904f57c908234c4fc78b473c75ce2a4ca19
Cokie Parker <cokie@parkertong.net>
2023-06-09 16:46:23 -0400

    single column update compiling and running. the timing seems to be wrong.

--- Algorithm0/Algorithm0/column_update.cu
+++ Algorithm0/Algorithm0/column_update.cu
@@ -3,13 +3,15 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 __global__ void ColumnUpdateKernel(int* colReads, int numHaps,
-    double probTransition, double probError, int* prevStateProbs, int* currStateProbs)
+    double probTransition, double probError, float* prevStateProbs, float* currStateProbs)
 {
     int i = threadIdx.x;
     for (int hap = 0; hap < numHaps; hap++) {
-        if (hap = i) {
+        if (hap == i) {
             currStateProbs[i] = (1 - probTransition) * probError;
         }
         else {
@@ -20,11 +22,11 @@ __global__ void ColumnUpdateKernel(int* colReads, int numHaps,
 
 // Helper function for using CUDA to add vectors in parallel.
 cudaError_t KernelTime(int* colReads, int numHaps,
-    double probTransition, double probError, int* prevStateProbs, int* currStateProbs, double* timeSec)
+    double probTransition, double probError, float* prevStateProbs, float* currStateProbs, double* timeSec)
 {
     int* dev_reads = 0;
-    int* dev_prevStateProbs = 0;
-    int* dev_currStateProbs = 0;
+    float* dev_prevStateProbs = 0;
+    float* dev_currStateProbs = 0;
     cudaError_t cudaStatus;
 
     // Choose which GPU to run on, change this on a multi-GPU system.
@@ -67,7 +69,11 @@ cudaError_t KernelTime(int* colReads, int numHaps,
     }
 
     // Launch a kernel on the GPU with one thread for each element.
-    ColumnUpdateKernel << <1, numHaps >> > (dev_reads, numHaps, probTransition, probError, dev_prevStateProbs, dev_currStateProbs);
+    clock_t before = clock();
+    ColumnUpdateKernel<<< 1, numHaps >>>(dev_reads, numHaps, probTransition, probError, dev_prevStateProbs, dev_currStateProbs);
+    cudaDeviceSynchronize();
+    clock_t difference = clock() - before;
+    *timeSec = difference * 1000 / CLOCKS_PER_SEC;
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
@@ -106,13 +112,27 @@ int main()
     const int numTests = 300;
 
     const int seed = 4;
+    srand(seed);
 
-    const string outFile = "time.log";
+    char outFile[] = "time.log";
 
-    const int colSize = 5;
-    const int a[colSize];
-    for () {
+    const int numHaps = 10;
+    const float probTransition = 0.05;
+    const float probError = 0.005;
 
+    double timeSec;
+
+    float prevStateProbs[numHaps] = {};
+    float currStateProbs[numHaps] = {};
+    int colReads[numHaps] = {};
+
+    for (int i = 0; i < numHaps; i++) {
+        colReads[i] = rand() % 2;
     }
 
+    printf("print \n");
+
+    KernelTime(colReads, numHaps, probTransition, probError, prevStateProbs, currStateProbs, &timeSec);
+
+    printf("time: %f\n", timeSec);
 }
